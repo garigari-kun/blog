8adb345a4e9aa64c9517f01b23d4e7663f6fd72f
Keisuke Tsukamoto <keisuke.cs@gmail.com>
2016-04-05 10:20:18 +0900

    Added facebook comment functionaliry

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.conf import settings
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
 
@@ -9,6 +10,7 @@ def upload_location(instance, filename):
 
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default = 1)
     title = models.CharField(max_length = 120)
     slug = models.SlugField(unique = True, null = True, blank = True)
     image = models.ImageField(upload_to = upload_location,
