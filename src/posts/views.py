b1ae79609aa748cbcd63cc1e01241a8a7e5ef794
Keisuke Tsukamoto <keisuke.cs@gmail.com>
2016-04-05 09:33:45 +0900

    Added basic user permission functionality

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,7 +2,7 @@ from urllib.parse import quote_plus
 
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render, redirect
 
 from .forms import PostForm
@@ -29,7 +29,9 @@ def post_list(request):
     return render(request, 'post_list.html', context)
 
 def post_create(request):
-    form = PostForm(request.POST or None, request.FIELS or None)
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit = False)
         instance.save()
@@ -54,6 +56,8 @@ def post_detail(request, slug = None):
     return render(request, 'post_detail.html', context)
 
 def post_update(request, slug = None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug = slug)
     form = PostForm(request.POST or None, request.FILES or None, instance = instance)
     if form.is_valid():
@@ -71,6 +75,8 @@ def post_update(request, slug = None):
     return render(request, "post_form.html", context)
 
 def post_delete(request, slug = None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug = slug)
     instance.delete()
     messages.success(request, 'Successfully deleted')
